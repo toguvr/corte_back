dcff3cba0e4cc5455e0adeb77c91abf8bdc16d0e
Augusto <augustotf93@gmail.com>
2021-05-25 01:24:25 -0300

    subindo back

--- src/server.ts
+++ src/server.ts
@@ -4,4 +4,6 @@ import { http } from "./http";
 
 import "./websocket/client";
 
-http.listen(3333, () => console.log("Server is running on port 3333"));
+http.listen(process.env.PORT || 3333, () =>
+  console.log("Server is running on port 3333")
+);
